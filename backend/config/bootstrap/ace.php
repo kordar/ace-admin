1450b8dd8fe197fe9c76cc46a5c348d9bb5893f9
perpel <572821520@qq.com>
2017-06-03 21:38:08 +0800

    UU

--- backend/config/bootstrap/ace.php
+++
@@ -1,4 +0,0 @@
-<?php
-Yii::setAlias('@kordar', dirname(dirname(dirname(__DIR__))) . '/vendor/kordar');
-Yii::setAlias('@acedir', '@kordar/yii2-ace/src/libs');
-Yii::setAlias('@aceassets', '@kordar/yii2-ace/src/assets/ace/assets');
